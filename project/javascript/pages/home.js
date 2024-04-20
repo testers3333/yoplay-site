71dcc7fd7dfe3e8b41fdc36de2ae4471e42bdab6
testers3333 <114099236+testers3333@users.noreply.github.com>
2024-04-20 12:19:18 +0200

    Add files via upload

--- project/javascript/pages/home.js
+++ project/javascript/pages/home.js
@@ -2,12 +2,14 @@ let hamburger = document.querySelector(".hamburger");
 let header = document.querySelector(".header");
 let a = document.querySelectorAll(".navigation_link")
 let root = document.documentElement;
+let navbg = document.querySelector(".nav-burger");
 
 hamburger.addEventListener("click", () => {
     if (hamburger.classList.contains("enabled")) {
         hamburger.classList.remove("enabled");
         hamburger.classList.add("disabled");
         header.classList.remove("activated");
+        navbg.classList.remove("opened")
         for(ab of a) {
             ab.classList.remove("acti");
             ab.classList.add("a");
@@ -17,6 +19,7 @@ hamburger.addEventListener("click", () => {
         hamburger.classList.add("enabled");
         hamburger.classList.remove("disabled");
         header.classList.add("activated");
+        navbg.classList.add("opened")
         for(ab of a) {
             ab.classList.add("acti");
             ab.classList.remove("a");
